acbca4b766279721a8672a3a8bed905ecaaf27d5
Keerti Gangimalammanavar <keerti.hanumappagangimalammanavar@capgemini.com>
2025-06-25 10:51:11 +0000

    second commit

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,11 +1,13 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/Device",
+    "sap/m/MessageToast"
+], function (Controller, JSONModel, Device, MessageToast) {
     "use strict";
- 
+
     return Controller.extend("app.tableapp.controller.View1", {
- 
+
         onInit: function () {
             var oDate = new Date();
             var sDate = oDate.toISOString().split('T')[0];  // YYYY-MM-DD format
@@ -14,29 +16,29 @@ sap.ui.define([
             });
             this.getView().setModel(oModel);
         },
- 
+
         onGoPress: function () {
             var oDateInput = this.byId("date");
             var sDate = oDateInput.getValue();
-            var oToday = new Date();
-            var sToday = oToday.toISOString().split('T')[0];
- 
-            // Check if the selected date is today's date
-            if (sDate !== sToday) {
-                sap.m.MessageToast.show("Please select today's date.");
-                oDateInput.setValue(sToday);
+
+            // Get selected technology key from Select control
+            var selectedTech = this.byId("tech").getSelectedKey();
+            console.log("Selected Technology: " + selectedTech);
+            console.log("Date selected: " + sDate);
+
+            // Navigate using the router, passing the selected key (technology) and date
+            var oRouter = this.getOwnerComponent().getRouter();
+            oRouter.navTo("RouteDetail", {
+                idButton: selectedTech,
+                dateSelected: sDate  // Pass selected date
+            });
+        },
+
+        onAfterRendering: function () {
+            if (Device.system.phone) {
+                this.getView().byId("idButton").setWidth("100%");
             } else {
-                // Get selected technology key from Select control
-                var selectedTech = this.byId("tech").getSelectedKey();
-                console.log("Selected Technology: " + selectedTech);
-                console.log("Date selected: " + sDate);
- 
-                // Navigate using the router, passing the selected key (technology) and date
-                var oRouter = this.getOwnerComponent().getRouter();
-                oRouter.navTo("RouteDetail", {
-                    idButton: selectedTech,
-                    dateSelected: sDate  // Pass selected date
-                });
+                this.getView().byId("idButton").setWidth(); // Optional: you can set a default width if needed
             }
         }
     });
